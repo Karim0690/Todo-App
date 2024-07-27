534318a975659692354537120edf7f71f4a7b5ab
Your NameKarim0690 <alkar33m@gmail.com>
2024-07-27 18:45:40 +0300

    Todo-App

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use('*', (req,res)=>{
   res.status(404).json({message:`This Route ${req.originalUrl} Not Found`})
 });
 
+const atlasUri = 'mongodb+srv://alkar33m:ybEWh4aubxkOAKFs@cluster0.nhqxsto.mongodb.net/Todo'
 mongoose
-  .connect('mongodb://localhost:27017/Todo')
+  .connect(atlasUri)
   .then(() => {
     console.log('Connected to MongoDB');
   })
