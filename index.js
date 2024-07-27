bc2f9a4b4f9b0065bcb68d40e94dcafde4c61fc0
Your NameKarim0690 <alkar33m@gmail.com>
2024-07-27 18:33:19 +0300

    Todo-App

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use('/users', userRouter);
 app.use('/todos', todoRouter);
+app.use('*', (req,res)=>{
+  res.status(404).json({message:`This Route ${req.originalUrl} Not Found`})
+});
 
 mongoose
   .connect('mongodb://localhost:27017/Todo')
